9f8a7c01a3f884c16491b00219a7a8ed5309c110
Moritz Fromm <git@moritz-fromm.de>
2019-08-31 22:44:06 +0200

    Add links from the topic page

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -5,10 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Topic;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use App\Entity\Link;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class TopicController extends AbstractController
 {
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     /**
      * @Route("/topic", name="topic")
      */
@@ -30,20 +39,58 @@ class TopicController extends AbstractController
     /**
      * @Route("/topic/{id}", name="topic_show")
      */
-    public function show($id)
+    public function show($id, Request $request)
     {
         $topic = $this->getDoctrine()
             ->getRepository(Topic::class)
             ->find($id);
 
+        $link = new Link();
+        $link->setTopic($topic);
+
+        $linkForm = $this->createFormBuilder($link)
+            ->add('href', UrlType::class, [
+                'label' => 'Link hinzufügen',
+                'attr' => [
+                    'placeholder' => 'Link eingeben',
+                ],
+            ])
+            ->add('description', UrlType::class, [
+                'label' => 'Anmerkung',
+                'attr' => [
+                    'placeholder' => 'Anmerkung eingeben',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Link hinzufügen',
+                'attr' => [
+                    'class' => 'btn-success btn-block',
+                ],
+            ])
+            ->getForm();
+
+        $linkForm->handleRequest($request);
+
+        if ($linkForm->isSubmitted() && $linkForm->isValid() && $this->isGranted('ROLE_USER'))
+        {
+            $link = $linkForm->getNormData();
+            dump($link);
+            $this->em->persist($link);
+            $this->em->flush();
+        }
         if (!$topic) {
             throw $this->createNotFoundException(
                 'No topic found for id '.$id
             );
         }
 
+        $linkForm = $linkForm->createView();
+
         // or render a template
         // in the template, print things with {{ product.name }}
-        return $this->render('default/single-view.html.twig', ['topic' => $topic]);
+        return $this->render('default/single-view.html.twig', [
+            'topic' => $topic,
+            'linkForm' => $linkForm,
+        ]);
     }
 }
