7dfe7de76682609f0f96a2b6f2ef66808d43de73
Moritz Fromm <git@moritz-fromm.de>
2019-08-31 22:17:20 +0200

    Display new topics on startpage

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\TopicRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    public function __construct(TopicRepository $topics)
+    {
+        $this->topics = $topics;
+    }
+
     /**
      * @Route("/", name="index")
      */
     public function index()
     {
-        return $this->render('default/index.html.twig');
+        $topics = $this->topics->findBy([], ['timestamp' => 'ASC']);
+
+        return $this->render('default/index.html.twig', [
+            'topics' => $topics,
+        ]);
     }
 
     /**
