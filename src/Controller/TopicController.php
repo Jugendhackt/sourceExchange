e5928debbfc281ad2592ad1796f343088587851e
Hendrik <hendrik-buerger@web.de>
2019-09-01 10:24:42 +0200

    Insert User-Alias into Database when creating a link.

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -4,26 +4,119 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\TopicRepository;
 use App\Entity\Topic;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use App\Entity\Link;
+use App\Entity\TopicUser;
+use Doctrine\DBAL\Types\TextType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType as SymfonyTextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class TopicController extends AbstractController
 {
-
-    public function __construct(TopicRepository $topics)
+    public function __construct(EntityManagerInterface $em)
     {
-      $this->topic = $topics;
+        $this->em = $em;
     }
 
     /**
-     * @Route("/topic/{topic}", name="topic")
+     * @Route("/topic", name="topic")
      */
-    public function index(Topic $topic)
+    public function index()
     {
-        //dump($topic);
-        return $this->render('topic/index.html.twig', [
+        return $this->render('index.html.twig', [
             'controller_name' => 'TopicController',
-            'topic' => $topic
+        ]);
+    }
+
+    /**
+     * @Route("/mytopics", name="mytopics")
+     */
+    public function mytopics()
+    {
+        return $this->render('default/mytopics.html.twig');
+    }
+
+    /**
+     * @Route("/topic/{id}", name="topic_show")
+     */
+    public function show($id, Request $request)
+    {
+        $topic = $this->getDoctrine()
+            ->getRepository(Topic::class)
+            ->find($id);
+
+        $topicUsername = $this->getDoctrine()
+            ->getRepository(TopicUser::Class)
+            ->find($this->getUser());
+
+        $link = new Link();
+        $link->setTopic($topic);
+
+
+        $preAliasList = array("Super","Ultra","xXpro","CatLover","Destroyer");
+        $postAliasList = array("LP","GamerXx","99","Alpaka","42");
+
+        $userAlias = new TopicUser();
+        $userAlias->setTopicId($topic);
+
+        $linkForm = $this->createFormBuilder($link)
+            ->add('href', UrlType::class, [
+                'label' => 'Link hinzufügen',
+                'attr' => [
+                    'placeholder' => 'Link eingeben',
+                ],
+            ])
+            ->add('description', SymfonyTextType::class, [
+                'label' => 'Anmerkung',
+                'attr' => [
+                    'placeholder' => 'Anmerkung eingeben',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Link hinzufügen',
+                'attr' => [
+                    'class' => 'btn-success btn-block',
+                ],
+            ])
+            ->getForm();
+
+        $linkForm->handleRequest($request);
+
+        if ($linkForm->isSubmitted() && $linkForm->isValid() && $this->isGranted('ROLE_USER'))
+        {
+            $link = $linkForm->getNormData();
+            $link->setUser($this->getUser());
+            $this->em->persist($link);
+            $this->em->flush();
+
+            if ($topicUsername == null)
+            {
+                $pre = mt_rand(0,4);
+                $post = mt_rand(0,4);
+                
+                $topicUsername = "$preAliasList[$pre]$postAliasList[$post]";
+                $userAlias->setUser($this->getUser());
+                $userAlias->setUsername($topicUsername);
+                $this->em->persist($userAlias);
+                $this->em->flush();
+            }
+        }
+        if (!$topic) {
+            throw $this->createNotFoundException(
+                'No topic found for id '.$id
+            );
+        }
+
+        $linkForm = $linkForm->createView();
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        return $this->render('default/single-view.html.twig', [
+            'topic' => $topic,
+            'linkForm' => $linkForm,
         ]);
     }
 }
