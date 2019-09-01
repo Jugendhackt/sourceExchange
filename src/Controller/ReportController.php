7a603710f44df07f1507e8f3860da5d8e1eb6332
Moritz Fromm <git@moritz-fromm.de>
2019-09-01 10:44:02 +0200

    Add form when reporting a link or topic

--- src/Controller/ReportController.php
+++ src/Controller/ReportController.php
@@ -9,6 +9,10 @@ use App\Entity\Report;
 use App\Entity\Topic;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReportController extends AbstractController
 {
@@ -21,18 +25,29 @@ class ReportController extends AbstractController
      * @IsGranted("ROLE_USER")
      * @Route("/report/new/topic/{topic}", name="report_topic")
      */
-    public function newReportFromTopic(Topic $topic)
+    public function newReportFromTopic(Topic $topic, Request $request)
     {
-        $report = new Report();
-        $report->setUser($this->getUser());
-        $report->setTopic($topic);
-        $this->em->persist($report);
-        $this->em->flush();
+        $form = $this->_generateReportForm($topic);
+        $form->handleRequest($request);
 
-        $this->addFlash('success', 'Meldung wurde erfolgreich erstellt');
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $report = $form->getNormData();
+            $report->setUser($this->getUser());
+            $report->setTopic($topic);
+            $this->em->persist($report);
+            $this->em->flush();
 
-        return $this->redirectToRoute('topic_show', [
-            'id' => $topic->getId()
+            $this->addFlash('success', 'Meldung wurde erfolgreich erstellt');
+            return $this->redirectToRoute('topic_show', [
+                'id' => $topic->getId()
+            ]);
+        }
+
+        $form = $form->createView();
+
+        return $this->render('report/new.html.twig', [
+            'form' => $form,
         ]);
     }
 
@@ -40,20 +55,35 @@ class ReportController extends AbstractController
      * @IsGranted("ROLE_USER")
      * @Route("/report/new/link/{link}", name="report_link")
      */
-    public function newReportFromLink(Link $link)
+    public function newReportFromLink(Link $link, Request $request)
     {
-        $report = new Report();
-        $report->setUser($this->getUser());
-        $report->setLink($link);
-        $this->em->persist($report);
-        $this->em->flush();
+        $topic = $link->getTopic();
 
-        $this->addFlash('success', 'Meldung wurde erfolgreich erstellt');
+        $form = $this->_generateReportForm($topic, $link);
+        $form->handleRequest($request);
 
-        return $this->redirectToRoute('topic_show', [
-            'id' => $link->getTopic()->getId(),
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $report = $form->getNormData();
+            $report->setUser($this->getUser());
+            $report->setTopic($topic);
+            $report->setLink($link);
+            $this->em->persist($report);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Meldung wurde erfolgreich erstellt');
+            return $this->redirectToRoute('topic_show', [
+                'id' => $topic->getId()
+            ]);
+        }
+
+        $form = $form->createView();
+
+        return $this->render('report/new.html.twig', [
+            'form' => $form,
         ]);
     }
+
 
     /**
      * @Route("/report/", name="report")
@@ -75,5 +105,42 @@ class ReportController extends AbstractController
         return $this->render('report/view.html.twig', [
             'report' => $report,
         ]);
+    }
+
+    private function _generateReportForm(?Topic $topic = null, ?Link $link = null)
+    {
+        $report = new Report();
+        $form = $this->createFormBuilder($report)
+            ->add('description', TextareaType::class, [
+                'label' => 'Beschreibung'
+            ]);
+
+        if (!is_null($topic))
+        {
+            $form->add('topicName', TextType::class, [
+                'mapped' => false,
+                'disabled' => 'true',
+                'label' => 'Name des Themas',
+                'data' => $topic->getTitle()
+            ]);
+        }
+
+        if (!is_null($link))
+        {
+            $form->add('linkUrl', TextType::class, [
+                'mapped' => false,
+                'disabled' => 'true',
+                'label' => 'Betroffener Link',
+                'data' => $link->getHref()
+            ]);
+        }
+
+        $form->add('submit', SubmitType::class, [
+            'label' => 'Anbsenden',
+            'attr' => [
+                'class' => 'btn-outline-warning btn-block'
+            ]]);
+
+        return $form->getForm();
     }
 }
